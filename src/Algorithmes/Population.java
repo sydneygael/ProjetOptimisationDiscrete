0211e7d6c6624f845766853f63d929cd75d4a946
Sydney ADJOU MOUMOUNI <sydgael@gmail.com>
2016-04-24 15:48:45 +0200

    ajout population et roulette

---
+++ src/Algorithmes/Population.java
@@ -0,0 +1,87 @@
+package Algorithmes;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * une population représente un ensemble de solution
+ * @author Sydney
+ *
+ */
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import Formation.Carte;
+import Formation.CentreFormation;
+import Utils.Utilitaires;
+
+/**
+ * 
+ * @author Sydney
+ *
+ */
+
+public class Population {
+
+	private List<Solution> populationDeSolutions;
+	private Solution meilleurSolution;
+	private int taille;
+	private Random alea;
+	private Carte carte ;
+
+	public Population(int taille) throws IOException {
+		this.taille = taille;
+		alea = new Random();
+		carte = new Carte();
+		carte.getAgencesFromFile(Utilitaires.RESSOURCES_LISTE_AGENCES);
+		carte.getCentreFormationFromFile(Utilitaires.RESSOURCES_LIEUX_POSSIBLES);
+
+		populationDeSolutions = new ArrayList<Solution>();
+
+		for (int i=0; i<taille; i++) {
+			populationDeSolutions.add(Utilitaires.genererSolutionAleatoire());
+		}
+
+		calculerMeilleurSolution();
+	}
+
+	public Solution getMeilleurSolution() {
+		return meilleurSolution;
+	}
+
+	public void reproduire(int k) {
+		populationDeSolutions = new Roulette(this, k).getNouvelleGeneration();
+	}
+
+	public void croiser() {
+
+		int n = populationDeSolutions.size();
+		for (int i=0; i+2<=n; i += 2) {
+			populationDeSolutions.get(i)
+			.croisement(populationDeSolutions.get(i + 1));
+		}
+	}
+
+	public void mutation(int taille) {
+
+		for (int i=0; i<taille; i++) {
+			int index = alea.nextInt(populationDeSolutions.size());
+			CentreFormation c = carte.getListCentreFormation().remove(0);
+			carte.getListCentreFormation().add(null);// ajouter bonne solution
+			populationDeSolutions.get(index).mutation(c);
+		}
+	}
+
+	private void calculerMeilleurSolution() {
+		meilleurSolution = Collections.min(populationDeSolutions);
+	}
+
+	public List<Solution> getSolutions() {
+		return populationDeSolutions;
+	}
+
+	public int getTaille() {
+		return taille;
+	}
+}
---
+++ src/Algorithmes/Roulette.java
@@ -0,0 +1,77 @@
+package Algorithmes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 
+ * @author Sydney
+ *
+ */
+public class Roulette {
+    
+    private Population population;
+    private List<Solution> meilleuresSolutions;
+    private List<Float> probabilitesCumulees;
+    private int kPopulation;
+    private Random alea;
+
+    public Roulette (Population population, int keptPopulationSize) {
+        this.population = population;
+        this.kPopulation = keptPopulationSize;
+        meilleuresSolutions = new ArrayList<Solution>(population.getSolutions());
+        probabilitesCumulees = new ArrayList<Float>();
+        alea = new Random();
+        calculerLesProbabilitesCumulees();
+    }
+    
+    
+    public Solution tournerRoulette() {
+        float prob = alea.nextFloat();
+        
+        Solution solutionTiree = null;
+        int index = 0;
+        
+        for (Float probCumulee : probabilitesCumulees) {
+           
+        	if (prob <= probCumulee) {
+                solutionTiree = meilleuresSolutions.get(index);
+                break;
+            }   	
+            index++;
+        }
+        return solutionTiree;
+    }
+    
+    public List<Solution> getNouvelleGeneration() {
+    	
+        List<Solution> nouvelleGeneration = new ArrayList<Solution>();
+        
+        for (int i=0; i< population.getTaille(); i++) {
+            Solution solution = tournerRoulette();
+            nouvelleGeneration.add(solution);
+        }
+        
+        return nouvelleGeneration;
+    }
+    
+    private void calculerLesProbabilitesCumulees() {
+        Collections.sort(meilleuresSolutions);
+        meilleuresSolutions = meilleuresSolutions.subList(0, kPopulation);
+        
+        float totalFitness = 0;
+        for (Solution solution : meilleuresSolutions) {
+            totalFitness += 1 / solution.getFitness();
+        }
+
+        float fitnessCumulee = 0;
+        
+        for (Solution solution : meilleuresSolutions) {
+            fitnessCumulee += 1 / solution.getFitness();
+            probabilitesCumulees.add(fitnessCumulee / totalFitness);
+        }
+    }
+
+}
